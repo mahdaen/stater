6af4cc1b086034a67a2ea25edada380ecc15ccba
Nanang Mahdaen El Agung <mahdaen@mahdaen-book.local>
2015-06-02 15:07:10 +0700

    Updating seo requirements

--- model/home.js
+++ model/home.js
@@ -4,7 +4,7 @@
 
 /* Exporting Homepage Data */
 module.exports = {
-    title: 'HTML Template with NodeJS Server',
+    title: 'Free HTML Template built in NodeJS Server',
     welcome: 'Hello! Your ship is delivering your Static HTML through the sea.',
     extras: 'Thanks for using <strong>Stater</strong> as your ship. Enjoy!',
 }
